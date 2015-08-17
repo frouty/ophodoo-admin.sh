b26241579766f7a632537f83d72d78b41a706099
laurent FRANCOIS <francois.oph@gmail.com>
2015-08-17 16:47:40 +1100

    update

--- Copy of update_odoo.sh
+++ Copy of update_odoo.sh
@@ -6,31 +6,24 @@
 #suffixe=$(date +%F_%T) ---> 2015-02-07_10:20:37
 #cp -a /home/lfs/odoogoeen /usr/odoogoeen-$(date +F_%T)
 
-#sudo
+#sudoexit
 #user host=(as user) [NOPWD:] cmd
 #1er arg : user
 #2eme arg: machine sur lequel le droit de la ligne sont valables
 #3eme arg: utilisateur dont root prend les droits
 #4eme arg: les commandes aux quelles user aura droit
 #-----------------------
-HOMEDIR=${HOME}
+
 ##--
 SCRIPT_DIR='ophodoo-admin.sh'
 SERVER_NAME='odoogoeen'
-SERVER_PATH='/home/lof/ODOO'
 REPOSITORY_PATH='/home/lof'
+SERVER_PATH='/home/lof/ODOO'
 USER_NAME='openerp'
-echo "SERVER:$HOMEDIR/$SERVER_DIR/$SERVER_NAME"
-#CURRENT_USER=lof
-
-##--
-
-REPOSITORY=$HOMEDIR/$DIR_NAME
-PROD_BRANCH='devfromscratch70'
-UPSTREAM='upstream'
 
 SERVER_BCK='backup'
 ##--
+FILESTORE_PATH="$SERVER_PATH/$SERVER_NAME/openerp"
 FILESTORE_DIR=filestore
 ##--
 SUFFIXE=$(date +'%F_%T')
@@ -45,24 +38,28 @@ if [ "$EUID" -ne 0 ]; then
 fi
 ##--
 echo "BEFORE, MAKE A DUMP"
-cd $HOMEDIR/$SCRIPT_DIR
+cd $REPOSITORY_PATH/$SCRIPT_DIR
 ./dump-oo.sh
 
 ##--
-echo 'shutdown odoogoeen service'
+echo 'Shutdown odoogoeen service'
 service odoo-server stop
 
 ##--
 # Copy dir_server prod to dir_server prod last
-# so if there is a problem it's easy to go back
-rsync -avz --progress -h $HOMEDIR/$SERVER_DIR/$SERVER_NAME/ $HOMEDIR/$SERVER_DIR/$SERVER_NAME.last
-rm -r $HOMEDIR/$SERVER_DIR/$SERVER_NAME
+# so if there is a problem it's easy to go bac
+# rm the the prod server dir
+rsync -avz --progress -h $SERVER_PATH/$SERVER_NAME/ $SERVER_PATH/$SERVER_NAME.last
+rm -r $SERVER_PATH/$SERVER_NAME
 
 
 ##--
 # Update the server repository
 # Change to the branch you want
 # git checkout branch namebranch
+cd $REPOSITORY_PATH/$SERVER_NAME
+printf 
+
 # then copy the repository
 rsync -avz --progress -h $REPOSITORY/ $HOMEDIR/$SERVER_DIR/$SERVER_NAME
 ##--
--- oo.sh
+++ oo.sh
@@ -2,10 +2,11 @@
 HOME=${HOME}
 
 printf "Home:%s\n" "$HOME"
-
-
 read -p "Entrez le nom du repository sur lequel vous voulez travailler: " REP_NAME
-read -p "Entrez la branche sur laquelle vous voulez être: " BRANCH
+
+cd $HOME/$REP_NAME
+printf "\nVoici les branches de ce repository:\n" 
+git branch -v
+read -p "\nEntrez la branche sur laquelle vous voulez être: " BRANCH
 printf "\nVous voulez être sur la branch: %s du repository: %s" "$BRANCH" "$REP_NAME"
 
-if [
